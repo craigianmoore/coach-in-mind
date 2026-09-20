18c1b2790f06a203e78ea36ba518048a7721cea9
craigianmoore <craigianmoore@gmail.com>
2026-09-20 13:39:29 +1000

    Remove 'NOW' from footer text

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function HomePage() {
         </div>
 
         <p className="mt-2 text-xs text-white/50">
-          Shaping Australian football coaches&rsquo; futures, NOW
+          Shaping Australian football coaches&rsquo; futures
         </p>
       </div>
     </div>
